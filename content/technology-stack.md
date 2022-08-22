373d7b5fd7b999d2fb47c5df71b6fccc9fa3b022
sumiyurup <50587837+sumiyurup@users.noreply.github.com>
2022-08-22 08:55:08 +0530

    new changes

---
+++ content/technology-stack.md
@@ -0,0 +1,3 @@
+---
+layout: technology-stack
+---
---
+++ static/js/dt-catcher.js
@@ -0,0 +1,3 @@
+function executeGoogleSend()
+{gtag('event','contact',{event_category:'Other Forms',event_action:'submit'});}
+executeGoogleSend();
